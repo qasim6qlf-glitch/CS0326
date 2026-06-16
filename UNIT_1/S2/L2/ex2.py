16493fc77d5bdbab46737daca1ad3767b0e6b24f
qasim6qlf-glitch <qasim6qlf@gmail.com>
2026-06-16 16:54:42 +0200

    esercitazione S2 L2

---
+++ UNIT_1/S2/L2/ex2.py
@@ -0,0 +1,4 @@
+città = input("Inserisci il nome della tua città di origine:")
+animale = input("Inserisci il nome del tuo animale domestico:")
+nome_band= città + " " + animale
+print (f"Il nome della tua città potrebbe essere: {nome_band}")
